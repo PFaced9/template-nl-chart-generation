8f8692a3fda56230aa5f090e0f3a6981b8e4d27a
PFaced9 <anubhavguptaa09@gmail.com>
2024-01-23 11:07:02 +0530

    added region id n pro id

--- demo-config.ts
+++ demo-config.ts
@@ -1,5 +1,5 @@
 /** Relevance account details
  * Should be the same as what you used in `relevance login`
  */
-export const REGION = '';
-export const PROJECT = '';
+export const REGION = 'a84fe0f1aa0e-4d45-a149-b325065cb300';
+export const PROJECT = 'f1db6c';
